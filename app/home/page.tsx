e1ef88083e889c1537f80909e6518976a416439f
rimpuhwe <rimpuhwemanzi@gmail.com>
2026-02-20 10:39:29 +0200

    Refactor: Added the image src to the hero image

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
             transition={{ duration: 0.7, ease: "easeOut" }}
           >
             <Image
-              src="/hero-healthcare.svg"
+              src="https://res.cloudinary.com/dcgmi6w24/image/upload/v1771571672/ChatGPT_Image_Feb_20_2026_09_10_38_AM_nbmzo0.png"
               alt="Doctors, pharmacist, and patient interacting with the system"
               width={850}
               height={600}
